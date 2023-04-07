9d556389e782c600354459254a3d12f2999757b0
parv-07 <127398078+parv-07@users.noreply.github.com>
2023-04-08 02:04:57 +0530

    Update database.sql

--- database.sql
+++ database.sql
@@ -10,28 +10,45 @@ use post;             --   Herer we will now use the database post;
 --we have created a posts table in which we took a references from the table from the user_profile
 create table posts(post_id int(20) auto_increment primary key,
 date datetime default current_timestamp,
-user_id int(20), 
+user_id int(20),
 foreign key(user_id) references user_profile(u_id));
+--modify the post table;
+alter table posts add column post_desc varchar(255);
 
 --for comment we have make a table comment which has user_id reference from the user taable and post_id reference form the post table
 create table comment(c_id int auto_increment primary key, 
 user_id int(20),foreign key(user_id) references user_profile(u_id),
 post_id int(20),foreign key(post_id) references posts(post_id));
+--modify the comment table;
+alter table comment add column multi_comment int(20);
+alter table comment add foreign key (multi_comment) references comment(c_id);
+alter table comment add column comment_desc varchar(255);
 
---for comment within comment
 
-create table multicomment(mc_id int auto_increment primary key,
-comment_id int(20),foreign key(comment_id) references comment(c_id),
-user_id int(20),foreign key(user_id) references user_profile(u_id));
+--NEW TABLE FOR LIKE AND DISLIKES
+create table likes(id int(100) auto_increment primary key,
+like_dislike enum('like','dislike'),
+user_id int(20),
+foreign key(user_id) references user_profile(u_id),
+post_id int(20), foreign key(post_id) references posts(post_id),
+comment_id int(20),foreign key(comment_id) references comment(c_id));
 
---like a post
-create table post_Like (id int auto_increment primary key, 
-user_id int(20),foreign key(user_id) references user_profile(u_id),
-post_id int(20),foreign key(post_id) references posts(post_id), 
-islike boolean);
+-- --for comment within comment
 
---dislike a post
-create table post_Dislike (id int auto_increment primary key, 
-user_id int(20),foreign key(user_id) references user_profile(u_id),
-post_id int(20),foreign key(post_id) references posts(post_id), 
-isdislike boolean);
+-- create table multicomment(mc_id int auto_increment primary key,
+-- comment_id int(20),foreign key(comment_id) references comment(c_id),
+-- user_id int(20),foreign key(user_id) references user_profile(u_id));
+
+-- --like a post
+-- create table post_Like (id int auto_increment primary key, 
+-- user_id int(20),foreign key(user_id) references user_profile(u_id),
+-- post_id int(20),foreign key(post_id) references posts(post_id), 
+-- islike boolean);
+
+-- --dislike a post
+-- create table post_Dislike (id int auto_increment primary key, 
+-- user_id int(20),foreign key(user_id) references user_profile(u_id),
+-- post_id int(20),foreign key(post_id) references posts(post_id), 
+-- isdislike boolean);
+
+
